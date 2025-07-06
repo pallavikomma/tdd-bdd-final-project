fde8949f08657067171ae6407ec5bf007b9977d6
pallavikomma <kommapallavi23@gmail.com>
2025-07-06 08:37:02 -0400

    Add step for pressing button

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -145,3 +145,7 @@ def step_impl(context, name):
         )
     )
     assert(found)
+@then('I should not see "{name}" in the results')
+def step_impl(context, name):
+    element = context.driver.find_element_by_id('search_results')
+    assert(name not in element.text)
