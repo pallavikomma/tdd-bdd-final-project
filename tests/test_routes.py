9ab9a6ad9e38c2693bd1a0b8ac167a6336d52e5d
pallavikomma <kommapallavi23@gmail.com>
2025-07-06 07:05:32 -0400

    Add REST API test cases for Read, Update, Delete, List endpoints

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -174,6 +174,20 @@ class TestProductRoutes(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
         self.assertEqual(data["name"], test_product.name)
+    def test_update_product(self):
+        """It should Update an existing Product"""
+        # create a product to update
+        test_product = ProductFactory()
+        response = self.client.post(BASE_URL, json=test_product.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # update the product
+        new_product = response.get_json()
+        new_product["description"] = "unknown"
+        response = self.client.put(f"{BASE_URL}/{new_product['id']}", json=new_product)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_product = response.get_json()
+        self.assertEqual(updated_product["description"], "unknown")
 
     ######################################################################
     # Utility functions
