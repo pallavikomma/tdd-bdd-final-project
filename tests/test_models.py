b9169bc56b09f871f6c8952edd69e6259c2e5c6b
pallavikomma <kommapallavi23@gmail.com>
2025-07-06 04:12:52 -0400

    Add test case for listing all Products

--- tests/test_models.py
+++ tests/test_models.py
@@ -155,3 +155,14 @@ class TestProductModel(unittest.TestCase):
         products = Product.all()
         self.assertEqual(len(products), 5)  # Now 5 products in DB
 
+    def test_find_by_name(self):
+        """It should Find a Product by Name"""
+        products = ProductFactory.create_batch(5)
+        for product in products:
+            product.create()
+        name = products[0].name
+        count = len([product for product in products if product.name == name])
+        found = Product.find_by_name(name)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.name, name)
